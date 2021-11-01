7a8d2c2bf1a267d130568db4b27d07b14d9673a1
victor de souza magaldi <victmagaldi@hotmail.com>
2021-11-01 20:06:46 -0300

    configurando webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
