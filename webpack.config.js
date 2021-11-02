d91f1d9d91d6a63571b45af250f2cf2f06ca61fe
victor de souza magaldi <victmagaldi@hotmail.com>
2021-11-01 21:10:39 -0300

    static html

--- webpack.config.js
+++ webpack.config.js
@@ -21,16 +21,7 @@ module.exports = {
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-react"],
-                    },
-                },
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
+                use: "babel-loader",
             },
         ],
     },
