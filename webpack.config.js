ffe32344c97c8374fcb53977c6a417f0b906f0cf
victor de souza magaldi <victmagaldi@hotmail.com>
2021-11-01 21:48:15 -0300

    source map webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
     },
+    devtool: "eval-source-map",
     resolve: {
         extensions: [".js", ".jsx"],
     },
