d2d8b84d3a634f524c101a0bef9b71cce0d8f40e
victor de souza magaldi <victmagaldi@hotmail.com>
2021-11-02 22:14:46 -0300

    first Component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,24 @@
+export function RepositoryList() {
+    return (
+        <section className="respository-list">
+            <h1>Lista de repositórios</h1>
+            <ul>
+                <li>
+                    <strong>teste</strong>
+                    <p>Forms in react js</p>
+                    <a href="">Acessar Repositório</a>
+                </li>
+                <li>
+                    <strong>teste</strong>
+                    <p>Forms in react js</p>
+                    <a href="">Acessar Repositório</a>
+                </li>
+                <li>
+                    <strong>teste</strong>
+                    <p>Forms in react js</p>
+                    <a href="">Acessar Repositório</a>
+                </li>
+            </ul>
+        </section>
+    );
+}
