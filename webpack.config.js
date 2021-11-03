0ecc4b9965c43ab274ee0032cb954fa2e7885f41
victor de souza magaldi <victmagaldi@hotmail.com>
2021-11-02 21:59:56 -0300

    add sass in project

--- webpack.config.js
+++ webpack.config.js
@@ -31,9 +31,16 @@ module.exports = {
                 use: "babel-loader",
             },
             {
-                test: /\.css$/,
+                test: /\.s[ac]ss$/i,
                 exclude: /node_modules/,
-                use: ["style-loader", "css-loader"],
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
             },
         ],
     },
